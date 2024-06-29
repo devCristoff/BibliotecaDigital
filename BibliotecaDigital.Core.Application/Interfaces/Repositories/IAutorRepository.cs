6655edc398cc262dcfec6cb95f582ce9eb8c442d
cristofer001 <cristoferdelamota0105@gmail.com>
2024-06-28 23:17:07 -0400

    [NEW]: Add interfaces for autor feature

---
+++ BibliotecaDigital.Core.Application/Interfaces/Repositories/IAutorRepository.cs
@@ -0,0 +1,8 @@
+﻿using BibliotecaDigital.Core.Domain.Entities;
+
+namespace BibliotecaDigital.Core.Application.Interfaces.Repositories
+{
+    public interface IAutorRepository : IGenericRepository<Autor>
+    {
+    }
+}
