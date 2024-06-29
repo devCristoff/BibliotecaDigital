6b49139a319c4f555bacff63b170188cf08de7a7
costa <costa@DESKTOP-EOOHF5T>
2024-06-28 23:46:17 -0400

    ADD: Book

---
+++ BibliotecaDigital.Core.Application/Interfaces/AutorService.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using BibliotecaDigital.Core.Application.Interfaces.Repositories;
+using BibliotecaDigital.Core.Application.Interfaces.Services;
+using BibliotecaDigital.Core.Application.ViewModels.Autor;
+using BibliotecaDigital.Core.Domain.Entities;
+
+namespace BibliotecaDigital.Core.Application.Services
+{
+    public class BookService : GenericService<SaveBookViewModel, BookViewModel, Book>, IBookService
+    {
+        private readonly IBookRepository _BookRepository;
+        private readonly IMapper _mapper;
+
+        public AutorService(IBookRepository BookRepository, IMapper mapper) : base(BookRepository, mapper)
+        {
+            _BookRepository = bookRepository;
+            _mapper = mapper;
+        }
+    }
+}
---
+++ BibliotecaDigital.Core.Application/ViewModelsBook/ViewModelsBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaDigital.Core.Application.ViewModelsBook
+{
+    internal class ViewModelsBook
+    {
+    }
+}
