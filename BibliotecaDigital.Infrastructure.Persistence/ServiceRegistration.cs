c364423c6746cbc39f8b32ad0209afcc3c863592
cristofer001 <cristoferdelamota0105@gmail.com>
2024-06-28 23:17:53 -0400

    [NEW]: Add autor repository feature

---
+++ BibliotecaDigital.Infrastructure.Persistence/ServiceRegistration.cs
@@ -0,0 +1,33 @@
+﻿using BibliotecaDigital.Core.Application.Interfaces.Repositories;
+using BibliotecaDigital.Infrastructure.Persistence.Contexts;
+using BibliotecaDigital.Infrastructure.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BibliotecaDigital.Infrastructure.Persistence
+{
+    public static class ServiceRegistration
+    {
+        public static void AddPersistenceInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            #region Contexts
+            if (configuration.GetValue<bool>("UseInMemoryDatabase"))
+            {
+                services.AddDbContext<ApplicationContext>(options => options.UseInMemoryDatabase("ApplicationDb"));
+            }
+            else
+            {
+                services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                m => m.MigrationsAssembly(typeof(ApplicationContext).Assembly.FullName)));
+            }
+            #endregion
+
+            #region Repositories
+            services.AddTransient(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+            services.AddTransient<IAutorRepository, AutorRepository>();
+            #endregion
+        }
+    }
+}
