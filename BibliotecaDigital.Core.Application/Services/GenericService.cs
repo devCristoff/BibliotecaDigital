0ce4c4d446b5084fd9aee61c65864decfb22b3e5
cristofer001 <cristoferdelamota0105@gmail.com>
2024-06-28 22:44:47 -0400

    [NEW]: Add generic service for handle entity service

---
+++ BibliotecaDigital.Core.Application/Services/GenericService.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using BibliotecaDigital.Core.Application.Interfaces.Repositories;
+using BibliotecaDigital.Core.Application.Interfaces.Services;
+
+namespace BibliotecaDigital.Core.Application.Services
+{
+    public class GenericService<SaveViewModel, ViewModel, Entity> : IGenericService<SaveViewModel, ViewModel, Entity>
+         where SaveViewModel : class
+         where ViewModel : class
+         where Entity : class
+    {
+        private readonly IGenericRepository<Entity> _repository;
+        private readonly IMapper _mapper;
+
+        public GenericService(IGenericRepository<Entity> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public virtual async Task<SaveViewModel> Add(SaveViewModel vm)
+        {
+            Entity entity = _mapper.Map<Entity>(vm);
+            entity = await _repository.AddAsync(entity);
+
+            SaveViewModel entityVm = _mapper.Map<SaveViewModel>(entity);
+
+            return entityVm;
+        }
+
+        public virtual async Task<SaveViewModel> GetByIdSaveViewModel(int id)
+        {
+            var entity = await _repository.GetByIdAsync(id);
+            SaveViewModel vm = _mapper.Map<SaveViewModel>(entity);
+
+            return vm;
+        }
+
+        public virtual async Task<List<ViewModel>> GetAllViewModel()
+        {
+            var entityList = await _repository.GetAllAsync();
+
+            return _mapper.Map<List<ViewModel>>(entityList);
+        }
+
+        public virtual async Task Update(SaveViewModel vm, int id)
+        {
+            Entity entity = _mapper.Map<Entity>(vm);
+            await _repository.UpdateAsync(entity, id);
+        }
+
+        public virtual async Task Delete(int id)
+        {
+            var entity = await _repository.GetByIdAsync(id);
+            await _repository.DeleteAsync(entity);
+        }
+    }
+}
